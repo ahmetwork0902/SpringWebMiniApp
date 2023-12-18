84f8f97f7d011e75c6a1bb27d3422ea5dec6da02
ahmet <ahmetdavresh@gmail.com>
2023-12-18 22:43:28 +0300

    init commit

---
+++ src/main/java/ru/ahmetdavresh/springwebmini/SpringWebMiniApplication.java
@@ -0,0 +1,13 @@
+package ru.ahmetdavresh.springwebmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMiniApplication.class, args);
+    }
+
+}
