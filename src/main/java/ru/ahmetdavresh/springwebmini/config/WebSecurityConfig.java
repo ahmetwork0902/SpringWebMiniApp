0d0f4e2876fe54b3dfd693c79537a3829f22e0c2
ahmet <ahmetdavresh@gmail.com>
2023-12-19 00:53:41 +0300

    first-ver

--- src/main/java/ru/ahmetdavresh/springwebmini/config/WebSecurityConfig.java
+++ src/main/java/ru/ahmetdavresh/springwebmini/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.bobojonov.springwebtestapp.config;
+package ru.ahmetdavresh.springwebmini.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
